7454abad5ccfc96881ea90cbade4e08970520f1b
DanielMora <danielalejandrolmoratorres@gmail.com>
2023-10-24 17:57:36 -0400

    changes deploy connect database

--- config/database.js
+++ config/database.js
@@ -49,26 +49,13 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'blognetlify'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '0000'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
+        database: env('DATABASE_NAME', 'strapi-prueba'),
+        user: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'admin'),
+        ssl: env.bool('DATABASE_SSL', true),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     sqlite: {
       connection: {
@@ -90,3 +77,27 @@ module.exports = ({ env }) => {
     },
   };
 };
+
+// postgres: {
+//   connection: {
+//     connectionString: env('DATABASE_URL'),
+//     host: env('DATABASE_HOST', 'localhost'),
+//     port: env.int('DATABASE_PORT', 5432),
+//     database: env('DATABASE_NAME', 'strapi-prueba'),
+//     user: env('DATABASE_USERNAME', 'postgres'),
+//     password: env('DATABASE_PASSWORD', '19981129da'),
+//     ssl: env.bool('DATABASE_SSL', false) && {
+//       key: env('DATABASE_SSL_KEY', undefined),
+//       cert: env('DATABASE_SSL_CERT', undefined),
+//       ca: env('DATABASE_SSL_CA', undefined),
+//       capath: env('DATABASE_SSL_CAPATH', undefined),
+//       cipher: env('DATABASE_SSL_CIPHER', undefined),
+//       rejectUnauthorized: env.bool(
+//         'DATABASE_SSL_REJECT_UNAUTHORIZED',
+//         true
+//       ),
+//     },
+//     schema: env('DATABASE_SCHEMA', 'public'),
+//   },
+//   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+// },
