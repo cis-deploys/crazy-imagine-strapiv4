898bcb812a11642584a1e4cced07329c737eac1d
Maria Rodriguez <marialerodrio@gmail.com>
2023-11-01 12:05:04 -0400

    aws-upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,8 +6,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'blogadmin.s3.amazonaws.com'],
-          'media-src': ["'self'",'data:','blob:', 'market-assets.strapi.io', 'blogadmin.s3.amazonaws.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'blogadmin.s3.us-east-1.amazonaws.com'],
+          'media-src': ["'self'",'data:','blob:', 'market-assets.strapi.io', 'blogadmin.s3.us-east-1.amazonaws.com'],
           upgradeInsecureRequests: null,
         },
       },
--- config/plugins.js
+++ config/plugins.js
@@ -13,14 +13,17 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          baseUrl: env('AWS_URL'),
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
           params: {
             ACL: 'public-read', // <== set ACL to private
             // signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
             Bucket: env('AWS_BUCKET_NAME'),
           },
+        }
         },
         actionOptions: {
           upload: {},
@@ -29,30 +32,4 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // upload: {
-    //   provider: "aws-s3", 
-    //   providerOptions: {
-    //     accessKeyId: env("AWS_ACCESS_KEY_ID"),
-    //     secretAccessKey: env("AWS_ACCESS_SECRET"),
-    //     region: env("AWS_REGION"),
-    //     params: {
-    //       Bucket: env("AWS_BUCKET_NAME"),
-    //     },
-    //   },
-    // },
-    // upload: {
-    //   config: {
-    //     provider: 'cloudinary',
-    //     providerOptions: {
-    //       cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-    //       api_key: env('CLOUDINARY_API_KEY'),
-    //       api_secret: env('CLOUDINARY_API_SECRET'),
-    //     },
-    //     actionOptions: {
-    //       upload: {},
-    //       uploadStream: {},
-    //       delete: {},
-    //     },
-    //   },
-    // },
   });
