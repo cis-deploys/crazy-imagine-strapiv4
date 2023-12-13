59b7296051e6f2dba91e964187baa02c95f35fef
Maria Rodriguez <marialerodrio@gmail.com>
2023-12-13 12:29:21 -0400

    fix package.json versions of the libraries and settings in the plugins.js file

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,4 @@
 module.exports = ({ env }) => ({
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
-      sites: [
-        {
-          name: 'crazyimagine-nextjs',
-          id: "bca1cdb0-58d0-4803-8ca5-fd37db559742",
-          buildHook: "https://api.netlify.com/build_hooks/653ff609fe789327cc3e9588",
-          branch: 'main'
-        }
-      ]
-    }
-  },
     email: {
       provider: "sendgrid",
       providerOptions: {
@@ -43,30 +29,4 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    // upload: {
-    //   provider: "aws-s3", 
-    //   providerOptions: {
-    //     accessKeyId: env("AWS_ACCESS_KEY_ID"),
-    //     secretAccessKey: env("AWS_ACCESS_SECRET"),
-    //     region: env("AWS_REGION"),
-    //     params: {
-    //       Bucket: env("AWS_BUCKET_NAME"),
-    //     },
-    //   },
-    // },
-    // upload: {
-    //   config: {
-    //     provider: 'cloudinary',
-    //     providerOptions: {
-    //       cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-    //       api_key: env('CLOUDINARY_API_KEY'),
-    //       api_secret: env('CLOUDINARY_API_SECRET'),
-    //     },
-    //     actionOptions: {
-    //       upload: {},
-    //       uploadStream: {},
-    //       delete: {},
-    //     },
-    //   },
-    // },
   });
