19943c787309f3509d0e7eb8722736cc9b53db2d
mariacabriles29 <mecp2992@gmail.com>
2023-12-27 09:32:55 -0400

    feat: created the category-review table and the relationships with projects and reviews

---
+++ src/api/category-review/services/category-review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-review service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::category-review.category-review');
